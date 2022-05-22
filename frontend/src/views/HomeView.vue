d7578965aa35757bac47fbbb6cca38aa9e028c84
gopelkujo <sigantengnoval@gmail.com>
2022-05-23 03:21:02 +0700

    :lipstick: frontend: Update UI landing page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,15 +1,117 @@
 <template>
-  <hello-world />
+  <div
+    class="pa-0 ma-0"
+  >
+    <v-card
+      class="elevation-0 mx-auto my-5"
+      max-width="1292"
+      max-height="426"
+    >
+      <v-img
+        src="../assets/background-header-lp.png"
+      >
+        <v-card
+          class="elevation-0 ml-6 text-h2 white--text font-weight-bold text-center"
+          max-width="657"
+          max-height="121"
+          style="background: #2155CD; margin-top: 213px"
+        >
+          <p
+            class="mx-auto py-6 text-h2 white--text font-weight-bold"
+          >
+          Pendopo Direnovasi!
+          </p>
+        </v-card>
+      </v-img>
+    </v-card>
+    <v-container
+      class="d-flex flex-row mx-auto mt-16 elevation-0"
+      max-width="1292"
+    >
+      <v-card
+        class="elevation-0"
+        width="907px"
+      >
+        <v-card-title
+          class="text-center"
+        >
+          ARTIKEL
+        </v-card-title>
+        <v-card
+          class="d-flex flex-row  elevation-0"
+        >
+          <v-card
+            class="mr-5 elevation-0"
+            width="288"
+          >
+            <img src="../assets/post-1.png" alt="post 1">
+            <v-card-title
+              class="text-h4 text-center text-wrap"
+            >
+              JTK Juara 1 Lomba Blog
+            </v-card-title>
+          </v-card>
+          <v-card
+            class="mr-5 elevation-0"
+            width="288"
+          >
+            <img src="../assets/post-2.png" alt="post 1">
+            <v-card-title
+              class="text-h4 text-center text-wrap"
+            >
+              Jurnal Ilmiah Oleh Dosen JTK
+            </v-card-title>
+          </v-card>
+          <v-card
+            class="mr-5 elevation-0"
+            width="288"
+          >
+            <img src="../assets/post-3.png" alt="post 1">
+            <v-card-title
+              class="text-h4 text-center text-wrap"
+            >
+              Dunia Metaverse telah Release
+            </v-card-title>
+          </v-card>
+        </v-card>
+      </v-card>
+      <!-- <v-card
+        class="elevation-0 py-7 px-6"
+        width="346px"
+        height="516px"
+        color="#0AA1DD"
+      >
+        <v-card-title
+          class="d-flex flex-row white--text"
+        >
+          AGENDA
+          <a
+            text
+            plain
+            href="#"
+            class="white--text"
+          >
+            lebih banyak
+          </a>
+        </v-card-title>
+        <div
+          class="pa-0 ma-0 d-flex flex-row"
+        >
+          <img src="../assets/tgl-agenda.png" alt="Tanggal">
+          <p 
+            class="pa-0 ma-0 text-h5 font-weight-medium"
+          >
+            Seminar Dosen
+          </p>
+        </div>
+      </v-card> -->
+    </v-container>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'HomeView',
-
-    components: {
-      HelloWorld,
-    },
   }
 </script>
