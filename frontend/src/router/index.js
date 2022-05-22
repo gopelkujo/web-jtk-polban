db69bebed60346164f04d7e26c2f75c91dc4064b
Hasbi-DOM <muhamad.hasbi.tif419@polban.ac.id>
2022-05-22 20:08:56 +0700

    :sparkles: frontend: add new profil jurusan and profil prodi

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProfilJurusan from '../views/ProfilJurusan.vue'
+import ProfilProdi from '../views/ProfilProdi.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +19,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/profil-jurusan',
+    name: 'profiljurusan',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: ProfilJurusan
+  },
+  {
+    path: '/profil-prodi',
+    name: 'profilprodi',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: ProfilProdi
   }
 ]
 
---
+++ frontend/src/views/ProfilJurusan.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <v-card
+      class="
+        d-flex
+        blue
+        accent-4
+        align-center
+        justify-space-between
+        pa-4
+        mx-auto
+        mt-11
+      "
+      height="255px"
+      max-width="1334px"
+      outlined
+    >
+      <div class="ml-9 my-auto">
+        <img
+          src="../assets/logo_profil_jurusan.png"
+          alt="logo_profil_jurusan"
+        />
+      </div>
+      <div class="'text-${model}' text-h2 white--text mr-16">
+        PROFIL JURUSAN
+      </div>
+    </v-card>
+    <div class="mt-10 mx-auto" style="width: 939px">
+      <p class="text-justify text-center">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+      <p class="text-justify text-center font-weight-bold">Visi dan Misi</p>
+      <p class="text-justify text-center mb-0">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+      <p class="text-justify text-center mb-0">Misi :</p>
+      <p class="text-justify text-center mb-0">
+        1. sLorem Ipsum is simply dummy text of the
+      </p>
+      <p class="mb-0">2. sLorem Ipsum is simply dummy text of the</p>
+      <P> 3. dLorem Ipsum is simply dummy text of the </P>
+      <P class="font-weight-bold"> Profil </P>
+      <P class="text-justify text-center">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </P>
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from '../components/HelloWorld'
+
+// export default {
+//   name: 'HomeView',
+
+//   components: {
+//     HelloWorld,
+//   },
+// }
+</script>
