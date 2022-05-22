3d5b0b564ebea1ad65e80af82a58b84cf72f3347
GEMINTANG SANGKAJI FURQON <gemintang.sangkaji.tif419@polban.ac.id>
2022-05-23 02:22:29 +0700

    :sparkless: frontend:add new daftar prestasi, struktur mata kuliah, info kerja sama

---
+++ frontend/src/views/InfoKerjasama.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <v-card
+      class="
+        d-flex
+        blue
+        accent-4
+        align-center
+        justify-space-between
+        pa-4
+        mx-auto
+        mt-11
+      "
+      height="255px"
+      max-width="1334px"
+      outlined
+    >
+      <div class="ml-9 my-auto">
+        <img
+          src="../assets/logo_kerjasama.png"
+          alt="logo_kerjasama"
+        />
+      </div>
+      <div class="'text-${model}' text-h2 white--text mr-16">
+        INFO KERJA SAMA
+      </div>
+    </v-card>
+    <center>
+        <div style="margin-top:40px">
+            <h1>Kerja Sama Akademik</h1>
+        </div>
+    </center>
+    <hr style="border:2px solid black; background-color:black;margin-left:42%;margin-right:42%;margin-top:10px">
+    <div class="mt-10 mx-auto" style="width: 1325px">
+      <p class="text-justify text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Suspendisse nec sapien arcu. Nam consequat risus condimentum, 
+        tincidunt lectus et, posuere lectus. Sed placerat purus a augue posuere,  
+        a finibus massa gravida. Cras fermentum iaculis egestas. In consequat, 
+        ligula vitae sodales tincidunt, nulla felis tempor turpis, id finibus lorem lorem vel.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Suspendisse nec sapien arcu. Nam consequat risus condimentum, 
+        tincidunt lectus et, posuere lectus. Sed placerat purus a augue posuere,  
+        a finibus massa gravida. Cras fermentum iaculis egestas. In consequat, 
+        ligula vitae sodales tincidunt, nulla felis tempor turpis, id finibus lorem lorem vel.
+      </p>
+      <div>
+          <center>  
+            <ul style="list-style-type: none;display: grid;  grid-template-columns: auto auto auto;">
+                <li>
+                    <img
+                        src="../assets/logo_kerjasama_1.png"
+                        alt="logo_kerjasama"
+                    /> 
+                </li>
+                    <li>
+                    <img
+                        src="../assets/logo_kerjasama_2.png"
+                        alt="logo_kerjasama"
+                    /> 
+                </li>
+                <li>
+                    <img
+                        src="../assets/logo_kerjasama_3.png"
+                        alt="logo_kerjasama"
+                    /> 
+                </li>
+            </ul>
+          </center>
+      </div>
+      <center>
+        <div>
+            <h1>Kerja Sama Bisnis</h1>
+        </div>
+    </center>
+    <hr style="border:2px solid black; background-color:black;margin-left:42%;margin-right:42%;margin-top:10px;margin-bottom:40px">
+      <p class="text-justify text-center mb-0">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Suspendisse nec sapien arcu. Nam consequat risus condimentum, 
+        tincidunt lectus et, posuere lectus. Sed placerat purus a augue posuere,  
+        a finibus massa gravida. Cras fermentum iaculis egestas. In consequat, 
+        ligula vitae sodales tincidunt, nulla felis tempor turpis, id finibus lorem lorem vel.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Suspendisse nec sapien arcu. Nam consequat risus condimentum, 
+        tincidunt lectus et, posuere lectus. Sed placerat purus a augue posuere,  
+        a finibus massa gravida. Cras fermentum iaculis egestas. In consequat, 
+        ligula vitae sodales tincidunt, nulla felis tempor turpis, id finibus lorem lorem vel.
+      </p>
+            <div>
+          <center>  
+            <ul style="list-style-type: none;display: grid;  grid-template-columns: auto auto auto;">
+                <li>
+                    <img
+                        src="../assets/logo_kerjasama_1.png"
+                        alt="logo_kerjasama"
+                    /> 
+                </li>
+                    <li>
+                    <img
+                        src="../assets/logo_kerjasama_2.png"
+                        alt="logo_kerjasama"
+                    /> 
+                </li>
+                <li>
+                    <img
+                        src="../assets/logo_kerjasama_3.png"
+                        alt="logo_kerjasama"
+                    /> 
+                </li>
+            </ul>
+          </center>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from '../components/HelloWorld'
+
+// export default {
+//   name: 'HomeView',
+
+//   components: {
+//     HelloWorld,
+//   },
+// }
+</script>
