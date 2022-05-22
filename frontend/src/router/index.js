be32b93a52a2878a3054ea7362c7a5c15d98caf4
mrafikamil <rafikamilm1@gmail.com>
2022-05-23 02:04:43 +0700

    :sparkles: Add Artikel & Detail Page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProfilJurusan from '../views/ProfilJurusan.vue'
 import ProfilProdi from '../views/ProfilProdi.vue'
+import ArtikelView from '../views/ArtikelView.vue'
+import DetailArtikel from '../views/DetailArtikel.vue'
 
 Vue.use(VueRouter)
 
@@ -35,6 +37,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: ProfilProdi
+  },
+  {
+    path: '/artikel',
+    name: 'artikel',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: ArtikelView
+  },
+  {
+    path: '/detail-artikel',
+    name: 'detailartikel',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: DetailArtikel
   }
 ]
 
