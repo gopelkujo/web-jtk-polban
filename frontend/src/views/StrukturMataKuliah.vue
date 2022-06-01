b18f443ff9bd5029fbfc3c7126bc1f319639cb55
Hasbi-DOM <muhamad.hasbi.tif419@polban.ac.id>
2022-06-01 18:53:51 +0700

    :sparkles: FE: Struktur Mata Kuliah

--- frontend/src/views/StrukturMataKuliah.vue
+++ frontend/src/views/StrukturMataKuliah.vue
@@ -1,219 +1,128 @@
 <template>
+
   <div>
-    <v-card
-      class="
-        d-flex
-        blue
-        accent-4
-        align-center
-        justify-space-between
-        pa-4
-        mx-auto
-        mt-11
-      "
-      height="255px"
-      max-width="1334px"
-      outlined
-    >
-      <div class="ml-9 my-auto">
-        <img
-          src="../assets/logo_struktur_mk.png"
-          alt="logo_struktur_mk"
-        />
-      </div>
-      <div class="'text-${model}' text-h2 white--text mr-16">
-        STRUKTUR MATA KULIAH
-      </div>
-    </v-card>
-    <template>
-      <div>
-        <div style="margin-left: 10%; margin-top: 30px">
-          <h3>
-            Prodi D3
-          </h3  >
-        </div>
-        <center>
-        <div style="display: grid;  grid-template-columns: auto auto auto;gap: 20px 200px;">
-            <ul style="list-style-type: none;">
-              <li><h4>Semester 1</h4></li>
-              <li>Matkul 1</li>
-              <li>Matkul 2</li>
-              <li>Matkul 3</li>
-              <li>Matkul 4</li>
-            </ul>
-            <ul style="list-style-type: none;">
-              <li><h4>Semester 2</h4></li>
-              <li>Matkul 1</li>
-              <li>Matkul 2</li>
-              <li>Matkul 3</li>
-              <li>Matkul 4</li>
-            </ul>
-            <ul style="list-style-type: none;">
-              <li><h4>Semester 3</h4></li>
-              <li>Matkul 1</li>
-              <li>Matkul 2</li>
-              <li>Matkul 3</li>
-              <li>Matkul 4</li>
-            </ul>
-            <ul style="list-style-type: none;">
-              <li><h4>Semester 4</h4></li>
-              <li>Matkul 1</li>
-              <li>Matkul 2</li>
-              <li>Matkul 3</li>
-              <li>Matkul 4</li>
-            </ul>
-            <ul style="list-style-type: none;">
-              <li><h4>Semester 5</h4></li>
-              <li>Matkul 1</li>
-              <li>Matkul 2</li>
-              <li>Matkul 3</li>
-              <li>Matkul 4</li>
-            </ul>
-            <ul style="list-style-type: none;">
-              <li><h4>Semester 6</h4></li>
-              <li>Matkul 1</li>
-              <li>Matkul 2</li>
-              <li>Matkul 3</li>
-              <li>Matkul 4</li>
-            </ul>
-          </div>
-        </center>
-        </div>
-        <hr style="margin: 60px">
-              <div>
-        <div style="margin-left: 10%; margin-top: 30px">
-          <h3>
-            Prodi D4
-          </h3  >
-        </div>
-        <center>
-        <div style="display: grid;  grid-template-columns: auto auto auto auto;gap: 20px;">
-            <ul style="list-style-type: none;">
-              <li><h4>Semester 1</h4></li>
-              <li>Matkul 1</li>
-              <li>Matkul 2</li>
-              <li>Matkul 3</li>
-              <li>Matkul 4</li>
-            </ul>
-            <ul style="list-style-type: none;">
-              <li><h4>Semester 2</h4></li>
-              <li>Matkul 1</li>
-              <li>Matkul 2</li>
-              <li>Matkul 3</li>
-              <li>Matkul 4</li>
-            </ul>
-            <ul style="list-style-type: none;">
-              <li><h4>Semester 3</h4></li>
-              <li>Matkul 1</li>
-              <li>Matkul 2</li>
-              <li>Matkul 3</li>
-              <li>Matkul 4</li>
-            </ul>
-            <ul style="list-style-type: none;">
-              <li><h4>Semester 4</h4></li>
-              <li>Matkul 1</li>
-              <li>Matkul 2</li>
-              <li>Matkul 3</li>
-              <li>Matkul 4</li>
-            </ul>
-            <ul style="list-style-type: none;">
-              <li><h4>Semester 5</h4></li>
-              <li>Matkul 1</li>
-              <li>Matkul 2</li>
-              <li>Matkul 3</li>
-              <li>Matkul 4</li>
-            </ul>
-            <ul style="list-style-type: none;">
-              <li><h4>Semester 6</h4></li>
-              <li>Matkul 1</li>
-              <li>Matkul 2</li>
-              <li>Matkul 3</li>
-              <li>Matkul 4</li>
-            </ul>
-            <ul style="list-style-type: none;">
-              <li><h4>Semester 7</h4></li>
-              <li>Matkul 1</li>
-              <li>Matkul 2</li>
-              <li>Matkul 3</li>
-              <li>Matkul 4</li>
-            </ul>
-            <ul style="list-style-type: none;">
-              <li><h4>Semester 8</h4></li>
-              <li>Matkul 1</li>
-              <li>Matkul 2</li>
-              <li>Matkul 3</li>
-              <li>Matkul 4</li>
-            </ul>
-          </div>
-        </center>
-        </div>
-        
-        <!-- <v-container
-          v-for="align in alignments"
-          :key="align"
-          class="grey lighten-5 mb-6"
+    <v-card>
+      <v-img src="../assets/struktur_mata_kuliah 1.png"
+      class="white--text align-end"
+      >
+
+        <v-card
+        color="#165687"
+        maxWidth="595"
+        maxheight="72"
+        class="white--text"
         >
-          <v-row
-            :align="align"
-            no-gutters
-            style="height: 150px;"
-          >
-            <v-col
-              v-for="n in 4"
-              :key="n"
-            >
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-                Matkul 1
-              </v-card>
+          <v-card-title
+          ><h3 class="font-weight-medium">STRUKTUR MATA KULIAH</h3></v-card-title>
+        </v-card>
+      </v-img>
+    </v-card>
+    
+      <v-card 
+      maxWidth="1400"
+      class="mx-auto mt-3 elevation-0 corner-radius-0"
+      >
+      <v-card
+    maxWidth="306"
+    maxHeight="38"
+    class="elevation-0 ml-0"
+    >
+      <v-select
+    :items="kurikulum"
+    label="Kurikulum"
+    outlined
+    >
+    </v-select>
+    </v-card>
+      <v-card-title class="mt-3"><h5>D-III Teknik Informatika</h5></v-card-title>
+        <v-table>
+        <v-data-table
+         dense
+        :headers="headers"
+        :items="no"
+        item-key="no"
+        class="elevation-1"
+        >
+        </v-data-table>
 
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-                Matkul 2
-              </v-card>
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-                Matkul 3
-              </v-card>
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-                Matkul 4  
-              </v-card>                            
-            </v-col>
-          </v-row>
-        </v-container>
-
-        <v-container class="grey lighten-5">
-          <v-row
-            no-gutters
-            style="height: 150px;"
-          >
-            <v-col
-              v-for="align in alignments"
-              :key="align"
-              :align-self="align"
-            >
-            </v-col>
-          </v-row>
-        </v-container>
-        </div>-->
-      
-    </template> 
-  </div>
+        <v-card-title><h5>D-IV Teknik Informatika</h5></v-card-title>
+        </v-table>
+        <v-table>
+        <v-data-table
+         dense
+        :headers="headers"
+        :items="no"
+        item-key="no"
+        class="elevation-1"
+        >
+        </v-data-table>
+      </v-table>
+      </v-card>
+    </div>
 </template>
 
 <script>
+export default {
+    data: () => ({
+      no: [
+        {
+          no: 1,
+          semester: 1,
+          kodeMataKuliah: '16TIN6001',
+          namaMataKuliah: 'Dasar Pemrograman',
+        },
+        {
+          no: 2,
+          semester: 1,
+          kodeMataKuliah: '16TIN6001',
+          namaMataKuliah: 'Aljabar Linear',
+        },
+        {
+          no: 3,
+          semester: 1,
+          kodeMataKuliah: '16TIN6001',
+          namaMataKuliah: 'Dasar Pemrograman',
+        },
+        {
+          no: 4,
+          semester: 1,
+          kodeMataKuliah: '16TIN6001',
+          namaMataKuliah: 'Dasar Pemrograman',
+        },
+        {
+          no: 5,
+          semester: 1,
+          kodeMataKuliah: '16TIN6001',
+          namaMataKuliah: 'Dasar Pemrograman',
+        },
+        {
+          no: 6,
+          semester: 1,
+          kodeMataKuliah: '16TIN6001',
+          namaMataKuliah: 'Dasar Pemrograman',
+        },
+        {
+          no: 7,
+          semester: 1,
+          kodeMataKuliah: '16TIN6001',
+          namaMataKuliah: 'Dasar Pemrograman',
+        },
+      ],
+      headers: [
+        {
+          text: 'No',
+          align: 'start',
+          sortable: false,
+          value: 'no',
+        },
+        { text: 'Semester', value: 'semester' },
+        { text: 'Kode Mata Kuliah', value: 'kodeMataKuliah' },
+        { text: 'Nama Mata Kuliah', value: 'namaMataKuliah' },
+      ],
+    }),
 
+    data1: () => ({
+     kurikulum: ['Kurikulum 2016', 'Bar', 'Fizz', 'Buzz'],
+    }),
+  }
 </script>
